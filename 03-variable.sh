9b2be05fd38b1ed367c25e18fa65b8427cda8f31
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-10 15:02:04 +0530

    actions

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hai, $PERSON2 How are you"
+echo "$PERSON2: Hai, $PERSON1 Iam good.What about you"
+echo "$PERSON1: Iam also good, What are you doing"
+echo "$PERSON2: Iam searching for job, What about you"
+echo "$PERSON1: Iam learning DevSecOps with AWS"
+echo "$PERSON2: Nice"
