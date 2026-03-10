feb5c6e560bec79c0ce319f07bb24eb7cebcc8ea
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-10 14:32:46 +0530

    actions

---
+++ 01-convesration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Anesh: Hai, Naga How are you"
+echo "Naga: Hai, Anesh Iam good.What about you"
+echo "Anesh: Iam also good, What are you doing"
+echo "Naga: Iam searching for job, What about you"
+echo "Anesh: Iam learning DevSecOps with AWS"
