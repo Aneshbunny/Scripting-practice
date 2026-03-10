fb440a1b24de25655951369a5fdcca701cbb4db6
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-10 16:54:54 +0530

    actions

--- 08-variable.sh
+++ 08-variable.sh
@@ -3,5 +3,5 @@ START_TIME=$(date +%s)
 echo "script excueted at:$START_TIME"
 sleep 50
 END_TIME=$(date +%s)
-TOTAL_TIME=(($START_TIME-$END_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script excueted in:$TOTAL_TIME
