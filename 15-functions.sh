56e8310f0fa747fbd83362cc2a8c6b3c37cf8273
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-11 12:41:30 +0530

    actions

---
+++ 15-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ];then
+ echo " Please run this script with root user access:"
+ exit 1
+fi
+VALIDATE(){
+   if [ $1 -ne 0 ];then
+     echo "$2...Failure"
+     exit 1
+    else
+     echo "$2...success"
+    fi
+}
+ echo "Installing Nginx"
+ dnf install nginx -y
+ VALIDATE $? "Installing Nginx"
+
+ echo "Installing Mysql"
+ dnf install mysql -y
+ VALIDATE $? "Installing Mysql"
+
+ echo "Installing Nodejs"
+ dnf install nodejs -y
+ VALIDATE $? "Installing Nodejs"
