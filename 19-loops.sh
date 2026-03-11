840572d15de345af6d44e5c382f44102c55c7f01
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-11 15:06:17 +0530

    actions

--- 19-loops.sh
+++ 19-loops.sh
@@ -9,10 +9,10 @@ fi
 mkdir -p $LOGS_FOLDER
 VALIDATE(){
    if [ $1 -ne 0 ];then
-     echo "$2...Failure"
+     echo "$2...Failure" | tee -a $LOGS_FILE
      exit 1
     else
-     echo "$2...success"
+     echo "$2...success" | tee -a $LOGS_FILE
     fi
 }
 
