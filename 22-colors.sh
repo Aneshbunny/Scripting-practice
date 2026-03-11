939b92481f80f3a8f0a1dce315ee3a01150272b8
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-11 16:47:33 +0530

    actions

--- 22-colors.sh
+++ 22-colors.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+PERSON1=$1
+PERSON2=$2
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
-N="\e[34m"
 
-echo "$RAnesh:Hai, Naga How are you"
-echo "$GNaga: Hai, Anesh Iam good.What about you"
-echo "$RAnesh: Iam also good, What are you doing"
-echo "$GNaga: Iam searching for job, What about you"
-echo "$RAnesh: Iam learning DevSecOps with AWS"
-echo "$GNaga: Nice"
+echo -e "$PERSON1: $RHai$N, $PERSON2 How are you"
+echo "$PERSON2: Hai, $PERSON1 Iam good.What about you"
+echo "$PERSON1: Iam also good, What are you doing"
+echo "$PERSON2: Iam searching for job, What about you"
+echo "$PERSON1: Iam learning DevSecOps with AWS"
+echo "$PERSON2: Nice"
