1bb87528d1675adb97b51edb9f45afc396041538
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-11 16:42:28 +0530

    actions

---
+++ 22-colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+echo "$RAnesh:Hai, Naga How are you"
+echo "$GNaga: Hai, Anesh Iam good.What about you"
+echo "$RAnesh: Iam also good, What are you doing"
+echo "$GNaga: Iam searching for job, What about you"
+echo "$RAnesh: Iam learning DevSecOps with AWS"
+echo "$GNaga: Nice"
