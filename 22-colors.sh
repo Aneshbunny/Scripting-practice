4ad88accf9b7c70f8042984b92a6c5d81806a672
Aneshbunny <iamaneshathkuri@gmail.com>
2026-03-11 16:50:10 +0530

    actions

--- 22-colors.sh
+++ 22-colors.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-echo -e "$PERSON1: $RHai$N, $PERSON2 How are you"
+echo -e "$PERSON1: $R Hai $N, $PERSON2 How are you"
 echo "$PERSON2: Hai, $PERSON1 Iam good.What about you"
 echo "$PERSON1: Iam also good, What are you doing"
 echo "$PERSON2: Iam searching for job, What about you"
